e3e6f0c293e3d2ce8b802ab51f094648c9dace2e
Neil Lawrence <lawrennd@gmail.com>
2024-12-14 21:54:43 +0000

    Add test of kappenball

---
+++ _reflections/kappenball.md
@@ -0,0 +1,90 @@
+---
+layout: reflection
+title: Kappenball
+date: 2024-12-14
+contributed_by:
+  initial:
+    type: "human"
+    name: "Neil D. Lawrence"
+    person_id: "Neil D. Lawrence"
+    date: "2024-12-15"
+    notes: "Written and approved for publication."
+---
+
+<!-- style for kappenball canvas-->
+<style>
+
+:root {
+    --kappenball-ground: rgba(56, 256, 56, 0.8);
+    --kappenball-pin: rgba(256, 56, 56, 0.8);
+    --kappenball-ball: rgba(200, 200, 200, 0.8);
+    --kappenball-membrane: rgba(56, 256, 56, 0.8);
+    --kappenball-hot: rgba(256, 56, 56, 0.8);
+    --kappenball-cold: rgba(56, 56, 256, 0.8);
+}
+
+body {
+  background-color: whiteg;
+  text-align: center;
+  font-family: Ubuntu Mono;
+}
+
+#title {
+  color: black;
+  font-size: 200%;
+  font-style: normal;
+  margin: 1px;
+  border: 1px;
+}
+
+#balls {
+  margin-top: 5px;
+}
+
+#myCanvas {
+  margin-top: -20px;
+}
+
+section.footer {
+  color: black;
+  font-family: Ubuntu Mono;
+  font-style: normal;
+  font-size: small;
+}
+
+#disclaimer {
+  font-size: 74%;
+  color: gray;
+}
+</style>
+
+
+## Kappenball
+
+If you want to complete a task, should you do it now or should you put it off until tomorrow? Despite being told to not delay tasks, many of us are deadline driven. Why is this?
+
+Kappenball is a simple game that illustrates that this behaviour can be optimal. It is inspired by an example in stochastic optimal control by [Bert Kappen](https://www.snn.ru.nl/~bertk/). The game is as follows: you need to place a falling balloon into one of two holes, but if the balloon misses the holes it will pop on pins placed in the ground. In 'deterministic mode', the balloon falls straight towards the ground and the game is easy. You simply choose which hole to place the ball in, and you can start to place it there as soon as the ball appears at the top of the screen. The game becomes more interesting as you increase the uncertainty. In Kappenball, the uncertainty takes the form of the balloon being blown left and right as it falls. This movement means that it is not sensible to decide early on which hole to place the balloon in. A better strategy is to wait and see which hole the ball falls towards. You can then place it in that hole using less energy than in deterministic mode. Sometimes, the ball even falls into the hole on its own, and you don't have to expend any energy, but it requires some skill to judge when you need to intervene. For this system Bert Kappen has shown mathematically that the best solution is to wait until the ball is close to the hole before you push it in. In other words, you should be deadline driven.
+
+In fact, it seems here uncertainty is a good thing, because on average you'll get the ball into the hole with less energy (by playing intelligently, and being deadline driven!) than you do with 'deterministic mode'. It requires some skill to do this, more than the deterministic system, but by using your resources intelligently you can get more out of the system. However, if the uncertainty increases too much then regardless of your skill, you can't control the ball at all.
+
+This simple game explains many of the behaviours we exhibit in real life. If a system is completely deterministic, then we can make a decision early on and be sure that the ball will 'drop in the hole'. However, if there is uncertainty in a system, it can make sense to delay our decision making until we've seen how events 'pan out'. Be careful though, as we also see that when the uncertainty is large, if you don't have the resources or the skill to be deadline-driven the uncertainty can overwhelm you and events can quickly move beyond our control.
+
+<div>
+  <div style="width:900px;text-align:center;display:inline">
+    <span style="float:left;">Score: <output id="kappenball-score"></output></span>
+    <span style="float:right;">Energy: <output id="kappenball-energy"></output></span>
+    <div style="clear: both;"></div>
+  </div>
+  <canvas id="kappenball-canvas" width="900" height="500" style="display:inline;text-align:center"></canvas>
+  <div>
+    <input type="range" min="0" max="100" value="0" class="slider" id="kappenball-stochasticity" style="width:900px;"/>
+  </div>
+  <div>
+    <button id="kappenball-newball" style="text-align:right">New Ball</button>
+    <button id="kappenball-pause" style="text-align:right">Pause</button>
+  </div>
+  <output id="kappenball-count"></output>
+  <script src="/assets/js/ballworld.js"></script>
+  <script src="/assets/js/kappenball.js"></script>
+</div>
+
---
+++ assets/js/kappenball.js
@@ -0,0 +1,215 @@
+// Copyright (c) 2020 Neil D. Lawrence
+
+/**
+ * Kappenball game class that extends the base Game class
+ * Implements a game where balls fall and need to be guided into holes while avoiding obstacles
+ */
+class Kappenball extends Game {
+    /**
+     * Initialize the Kappenball game
+     * @param {Object} objects - Contains arrays of game objects (balls, boxes, pits, etc)
+     * @param {Object} params - Game parameters like gravity, elasticity, etc
+     * @param {Object} simulation - Simulation settings like pause state
+     * @param {Object} boundaries - Boundary behavior settings
+     * @param {Object} context - Canvas context for rendering
+     * @param {Object} colors - Color definitions for game objects
+     */
+    constructor(objects, params, simulation, boundaries, context, colors) {
+        super(objects, params, simulation, boundaries, context, colors);
+
+        // Width of holes that balls can fall into
+        this.holeWidth = 100;
+
+        // Create the game layout with boxes, pits and posts
+        // Left side pit and ground
+        this.objects.pits[this.objects.pits.length] = new Box(this.context, 0, this.context.canvas.height-40, 90, 30, this.colors.pin);
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 0, this.context.canvas.height-10, 90, 10, this.colors.ground);
+        
+        // First hole boundaries
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 95, this.context.canvas.height-35, 5, this.colors.ground);
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 90, this.context.canvas.height-30, 10, 20, this.colors.ground);
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 95, this.context.canvas.height-5, 5, this.colors.ground);
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 305, this.context.canvas.height-35, 5, this.colors.ground);
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 300, this.context.canvas.height-30, 10, 20, this.colors.ground);
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 305, this.context.canvas.height-5, 5, this.colors.ground);
+
+        // Middle section
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 310, this.context.canvas.height-10, 280, 10, this.colors.ground);
+        this.objects.pits[this.objects.pits.length] = new Box(this.context, 310, this.context.canvas.height-40, 280, 30, this.colors.pin);
+
+        // Second hole boundaries  
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 595, this.context.canvas.height-35, 5, this.colors.ground);
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 590, this.context.canvas.height-30, 10, 20, this.colors.ground);
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 595, this.context.canvas.height-5, 5, this.colors.ground);
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 805, this.context.canvas.height-35, 5, this.colors.ground);
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 800, this.context.canvas.height-30, 10, 20, this.colors.ground);
+        this.objects.posts[this.objects.posts.length] = new Post(this.context, 805, this.context.canvas.height-5, 5, this.colors.ground);
+
+        // Right side pit and ground
+        this.objects.pits[this.objects.pits.length] = new Box(this.context, 810, this.context.canvas.height-40, 750, 30, this.colors.pin);
+        this.objects.boxes[this.objects.boxes.length] = new Box(this.context, 810, this.context.canvas.height-10, 90, 10, this.colors.ground);
+    }
+
+    /**
+     * Increment the game score
+     * @param {number} amount - Amount to increment score by
+     */
+    incrementScore(amount) {
+        score.value = parseInt(score.value)+amount;
+    }
+
+    /**
+     * Increment the energy counter
+     * @param {number} accel - Acceleration value to add to energy
+     */
+    incrementEnergy(accel) {
+        energy.value = parseFloat(energy.value)+accel;
+    }
+
+    /**
+     * Create a new ball at the top center of the game
+     */
+    birth() {
+        var temp = new Ball(this.context, this.context.canvas.width/2, 10, 10);
+        temp.dx = 0;
+        temp.dy = 1;
+        this.objects.balls[this.objects.balls.length] = temp;
+    }
+
+    /**
+     * Reset the game by creating a new ball
+     */
+    reset() {
+        this.birth();
+    }
+}
+
+// Get DOM elements
+var slider = document.getElementById("kappenball-stochasticity");
+var score = document.getElementById("kappenball-score");
+var ballCount = document.getElementById("kappenball-count");
+var energy = document.getElementById("kappenball-energy");
+
+// Initialize counters
+score.value = 0
+ballCount.value = 0
+energy.value = 0
+
+// Set up button controls
+var newballKappenballButton = document.getElementById("kappenball-newball");
+var pauseKappenballButton = document.getElementById("kappenball-pause");
+
+newballKappenballButton.addEventListener("click", function() {
+    kappenball.reset()
+});
+pauseKappenballButton.addEventListener("click", function() {
+    kappenball.togglePause()
+});
+
+// Set up keyboard controls
+document.addEventListener("keydown", function() {
+     keyDownHandler(event, kappenball);
+});
+document.addEventListener("keyup", function() {
+    keyUpHandler(event, kappenball);
+});
+
+// Initialize stochasticity and handle slider changes
+var stochasticity = slider.value
+slider.oninput = function() {
+    kappenball.params.stochasticity = this.value;
+}
+
+// Define colors for game objects
+var colors = {
+    ground: 'rgba(56, 256, 56, 0.8)',    // Green
+    pin: 'rgba(256, 56, 56, 0.8)',       // Red
+    ball: 'rgba(200, 200, 200, 0.8)',    // Gray
+    membrane: 'rgba(56, 256, 56, 0.8)',  // Green
+    hot: 'rgba(256, 56, 56, 0.8)',       // Red
+    cold: 'rgba(56, 56, 256, 0.8)'       // Blue
+};
+
+// Simulation settings
+var simulation = {
+    paused: false,
+    gravity: true,
+    drag: true,
+    sound: false,
+    clearCanv: true,
+    bigBalls: false,
+    dt: 1
+};
+
+// Boundary behavior settings
+var boundaries = {
+    wallBounce: true,
+    floorBounce: false,
+    floorWrap: false,
+    floorWrapCenter: true,
+    floorReset: false
+};
+
+// Game physics parameters
+var params = {
+    inelasticityFactor: 1.0,
+    demonThreshold: 3,
+    initialSpeed: 5,
+    energy: 0.0,
+    gravityAccel: 0.06,
+    arrowAccel: 0.4,
+    stochasticity: 0.0,
+    stochasticityScale: 0.2,
+    dragFactor: 0.97
+};
+
+// Initialize game objects arrays
+var objects = {
+    balls: [],
+    boxes: [],
+    pits: [],
+    posts: [],
+    membranes: []
+};
+
+// Get canvas context
+var context = {
+    canvas: document.getElementById("kappenball-canvas")
+};
+
+// Create game instance
+kappenball = new Kappenball(objects, params, simulation, boundaries, context, colors);
+
+/**
+ * Handle click events on the canvas
+ * @param {Event} event - Click event
+ * @param {Kappenball} game - Game instance
+ */
+function clickReporter(event, game) {
+    const rect = game.context.canvas.getBoundingClientRect();
+    const backgroundColor = game.context.canvas.style.backgroundColor;
+    const x = event.clientX - rect.left;
+
+    // Visual feedback flash
+    game.context.canvas.style.backgroundColor = "rgb(0, 0, 0)";
+    game.draw();
+    game.context.canvas.style.backgroundColor = "rgb(255, 255, 255)";
+    game.draw();
+    game.context.canvas.style.backgroundColor = backgroundColor;
+
+    // Push ball left or right based on click position
+    if(x > game.context.canvas.width/2) {
+        game.pushLeft(20.0)
+    } else {
+        game.pushRight(20.0)
+    }
+}
+
+// Add click handler to canvas
+kappenball.context.canvas.addEventListener("click", function(event) {
+    clickReporter(event, kappenball);
+});
+
+// Start the game
+kappenball.reset();
+draw(kappenball);
